95bdbe5930d7a7089cd8076852caf8c4d19a11a9
oztalha <toz@gmu.edu>
2015-01-08 18:51:37 -0500

    lazy load problemi cozuldu

--- collectOverview.py
+++ collectOverview.py
@@ -5,16 +5,27 @@ Created on Thu Jan 08 15:41:01 2015
 @author: Talha
 """
 
-# coding: utf-8
 from splinter import Browser
 import time
+import pandas as pd
 
 browser = Browser('firefox')
 #Nediyor Turkiye haberleri sayfasi
 browser.visit("http://nediyor.com/haberler/turkiye/")
+
+#sayfanin sonuna git - her defasinda 18 yeni haber yukleniyor
+for i in range(100):
+    browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(5)
+
 main = browser.find_by_id("brick-wrap").first
-#links are titles: 1,13,25...
-haberler = main.find_by_xpath("//div[@class='brick brick-big']")
-print len(haberler)
-for haber in haberler:
-    print haber.find_by_tag("h2").find_by_tag("a").html
+news = main.find_by_xpath("//div[@class='brick brick-big']")
+df_news = pd.DataFrame(columns=('href', 'title', 'twcount'))
+for haber in news:
+    title = haber.find_by_tag("h2").find_by_tag("a")
+    href = title['href']
+    title = title.html
+    twcount = haber.find_by_xpath("div[@class='tweet-count']").first.find_by_tag("a").html
+    df_news.loc[len(df_news)+1]=[href,title,int(twcount)]
+    
+df_news.to_csv("haberler.csv",encoding='utf-8',index=False)
